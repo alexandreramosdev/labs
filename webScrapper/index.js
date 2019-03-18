b2b9e07ffc813681a118496df8f7270026a333ac
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2019-03-18 02:53:04 -0300

    create first webcrapping with puppetter

---
+++ webScrapper/index.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+let scrape = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  });
+
+  const page = await browser.newPage();
+  await page.goto("http://books.toscrape.com/");
+  await page.waitFor(1000);
+
+  // Scrape
+  const result = await page.evaluate(() => {
+    let books = [];
+    document.querySelectorAll("h3 > a").forEach(book => books.push(book.title));
+    return books;
+  });
+
+  browser.close();
+  return result;
+};
+
+scrape().then(value => {
+  console.log(value.length);
+});
